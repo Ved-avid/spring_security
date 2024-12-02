ed8eb2f92a0d622fefab531abae795b1d5f6418c
Ved Singh <64973965+Ved-avid@users.noreply.github.com>
2024-12-02 23:17:20 +0530

    controller , renamed the files

--- section1/springsection1/src/main/java/com/springwithved/springsection1/BankApplication.java
+++ section1/springsection1/src/main/java/com/springwithved/springsection1/BankApplication.java
@@ -2,8 +2,12 @@ package com.springwithved.springsection1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
+//If the compenent is not in the parent package then we can specify the path for spring to scan the component
+// @ComponentScan("com.springwithved.springsection1.controller")
 public class BankApplication {
 
 	public static void main(String[] args) {
---
+++ section1/springsection1/src/main/java/com/springwithved/springsection1/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.springwithved.springsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+
+    @GetMapping("/welcome")
+    public String welcomeWithoutSecurtiy(){
+        return "Welcome to Spring course without Security";
+    }
+}
